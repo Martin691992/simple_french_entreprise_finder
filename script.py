ed5b40ab98210188419ce801f523d5e7688d53bd
Martin BONNARD <bonnard.martin@gmail.com>
2025-06-09 11:21:15 +0200

    changement code

--- script.py
+++ script.py
@@ -23,7 +23,9 @@ import time
 #
 #
 
-requetebase = "https://recherche-entreprises.api.gouv.fr/search?page=1&per_page=25&categorie_entreprise=PME&departement=69,01,38,42&section_activite_principale=H&tranche_effectif_salarie=NN,00,01,02,03,11,12,21&etat_administratif=A&activite_principale=52.29A"
+activite_principale = '52.10B'
+name_file = '5210B'
+requetebase = f"https://recherche-entreprises.api.gouv.fr/search?page=1&per_page=25&categorie_entreprise=PME&departement=69,01,38,42&section_activite_principale=H&tranche_effectif_salarie=NN,00,01,02,03,11,12,21&etat_administratif=A&activite_principale={activite_principale}"
 data = requests.get(requetebase)
 response = data.json()
 
@@ -31,12 +33,12 @@ response = data.json()
 index=1    
 for a in range(int(response['total_pages'])):
     print(f'------------   Début de la page N°{index} ----------------------')
-    requete = f"https://recherche-entreprises.api.gouv.fr/search?page={index}&per_page=25&categorie_entreprise=PME&departement=69,01,38,42&section_activite_principale=H&tranche_effectif_salarie=NN,00,01,02,03,11,12,21&etat_administratif=A&activite_principale=52.29A"
+    requete = f"https://recherche-entreprises.api.gouv.fr/search?page={index}&per_page=25&categorie_entreprise=PME&departement=69,01,38,42&section_activite_principale=H&tranche_effectif_salarie=NN,00,01,02,03,11,12,21&etat_administratif=A&activite_principale={activite_principale}"
     data_fetch_loop = requests.get(requete)
     response_loop = data_fetch_loop.json()
     
     for i in response_loop['results'] :
-        with open('./results5229A.txt','a') as w:
+        with open(f'./results{name_file}.txt','a') as w:
             if i['nom_raison_sociale'] != None:
                 dirigeants_valides = [
                     f"{d.get('nom','')},{d.get('prenoms','')}"
@@ -44,11 +46,11 @@ for a in range(int(response['total_pages'])):
                     if 'nom' in d and 'prenoms' in d
                 ]
                 if dirigeants_valides:
-                    print(f"Raison Entreprise :{i['nom_raison_sociale']} ---- Dirigeants :  {' | '.join(dirigeants_valides)}\n")
-                    w.write(f"Raison Entreprise :{i['nom_raison_sociale']} ---- Dirigeants :  {' | '.join(dirigeants_valides)}\n")
+                    print(f"Raison Entreprise :{i['nom_raison_sociale']} | Dirigeants :  {' | '.join(dirigeants_valides)}\n")
+                    w.write(f"Raison Entreprise :{i['nom_raison_sociale']} | Dirigeants :  {' | '.join(dirigeants_valides)}\n")
                 else : 
-                    print(f"Raison Entreprise :{i['nom_raison_sociale']} ---- Pas de noms de dirigeants...\n")
-                    w.write(f"Raison Entreprise :{i['nom_raison_sociale']} ---- Pas de noms de dirigeants...\n")
+                    print(f"Raison Entreprise :{i['nom_raison_sociale']} | Dirigeants : Pas de noms de dirigeants...\n")
+                    w.write(f"Raison Entreprise :{i['nom_raison_sociale']} | Dirigeants : Pas de noms de dirigeants...\n")
 
                 # noms_dirigeant = [a for a in i['dirigeants']]
                 # print(f"Raison Entreprise :{i['nom_raison_sociale']} ---- Dirigeants :  {' | '.join([f"{d['nom']},{d['prenoms']}" for d in noms_dirigeant if 'nom' in d and 'prenoms' in d])}\n")
